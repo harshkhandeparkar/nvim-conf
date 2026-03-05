502c426b6cbd391a6d5ea183730b2961090ad4f4
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2026-03-05 18:37:00 +0530

    feat: whitespace chars

--- init.lua
+++ init.lua
@@ -12,6 +12,14 @@ vim.opt.scrolloff = 8
 -- Horizontal split splits below the current window
 vim.opt.splitbelow = true
 
+-- Display whitespaces
+vim.opt.list = true
+vim.opt.listchars = {
+  tab = "» ",
+  space = "·",
+  trail = "•",
+}
+
 require("remap")
 require("lazy_config")
 require("diagnostics")
