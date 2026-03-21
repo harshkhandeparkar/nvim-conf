e7e41ca060f569032a62f42e34b32ded13cac244
Harsh Khandeparkar <shadowwarriorpro3003@gmail.com>
2026-03-21 17:45:09 +0530

    feat: updated LSPs; ty is so fast it is refreshing

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -39,7 +39,8 @@ return {
 			vim.lsp.enable("rust_analyzer")
 			vim.lsp.enable("ts_ls")
 			vim.lsp.enable("lua_ls")
-			vim.lsp.enable("pyright")
+			vim.lsp.enable("ty")
+			vim.lsp.enable("zls")
 		end
 	}
 }
